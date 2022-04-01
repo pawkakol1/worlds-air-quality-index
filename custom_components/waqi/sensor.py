f39b67df1549b91e1c7689b084c4f9f4ad712525
pawkakol1 <pawel.kakol130@gmail.com>
2022-04-01 18:27:51 +0200

    Reset to state of Homeassistant Example

--- custom_components/waqi/sensor.py
+++ custom_components/waqi/sensor.py
@@ -1,23 +1,36 @@
 """Platform for sensor integration."""
 
 import logging
-from datetime import timedelta
-import json
-import requests
-import voluptuous as vol
+#from datetime import timedelta
+#import json
+#import requests
+#import voluptuous as vol
 
-from homeassistant.components.sensor import PLATFORM_SCHEMA
+"""from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import (CONF_API_KEY, CONF_LATITUDE, CONF_LONGITUDE, CONF_SCAN_INTERVAL)
 from homeassistant.util import Throttle
-from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity import Entity"""
 
+
+from __future__ import annotations
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import TEMP_CELSIUS
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 _LOGGER = logging.getLogger(__name__)
 
+"""
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_API_KEY): cv.string,
     vol.Required(CONF_LATITUDE): cv.string,
@@ -39,7 +52,7 @@ SENSORS = {
     't': ['Temperature', 'degC', 'mdi:thermometer'],
     'w': ['Wind speed', 'm/s', 'mdi:weather-windy'],
     'aqi': ['Air Quality Index', '', 'mdi:leaf']
-}
+}"""
 
 def setup_platform(
     hass: HomeAssistant,
@@ -47,7 +60,16 @@ def setup_platform(
     add_entities: AddEntitiesCallback,
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
-    """Set up the sensor platform."""
+    #Set up the sensor platform.
+    add_entities([WaqiSensor()])
+
+"""def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    #Set up the sensor platform.
     lat = config.get(CONF_LATITUDE)
     lng = config.get(CONF_LONGITUDE)
     apikey = config.get(CONF_API_KEY)
@@ -62,10 +84,10 @@ def setup_platform(
     result = []
     for res in SENSORS:
         result.append(WaqiSensor(data, res.lower()))
-    add_entities(result)
+    add_entities(result)"""
 
 
-class WaqiHttpRequester(object):
+"""class WaqiHttpRequester(object):
 
     def __init__(self, lat, lng, apikey, scanPeriod):
         self._state = None
@@ -84,13 +106,25 @@ class WaqiHttpRequester(object):
             _LOGGER.error("Error occurred while fetching data: %r", exc)
             self.data = None
             return False
+"""
 
+class WaqiSensor(SensorEntity):
+    #Representation of a Sensor.
 
-class WaqiSensor(Entity):
-    """Representation of a Sensor."""
+    _attr_name = "Air Quality Index"
+    _attr_device_class = SensorDeviceClass.AQI
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        #Fetch new state data for the sensor.
+        #This is the only method that should fetch new data for Home Assistant.
+        self._attr_native_value = 23
+
+"""class WaqiSensor(Entity):
+    #Representation of a Sensor.
 
     def __init__(self, data, sensor_type):
-        """Initialize the sensor."""
+        #Initialize the sensor.
         self.data = data
         self.type = sensor_type
         self._unit = SENSORS[self.type][1]
@@ -119,10 +153,9 @@ class WaqiSensor(Entity):
         return self._extra_state_attributes
 
     def update(self):
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
+        #Fetch new state data for the sensor.
+        #This is the only method that should fetch new data for Home Assistant.
+        
         self.data.update(self.type)
 
         inputData = self.data.data
@@ -161,3 +194,4 @@ class WaqiSensor(Entity):
 
         except ValueError:
             self._state = None
+"""

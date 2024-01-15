57ac1aa5a113b3d4a90df93333a4228566ad8d48
pawkakol1 <pawel.kakol130@gmail.com>
2024-01-15 20:36:06 +0100

    Update const.py
    Increase version

--- custom_components/worlds_air_quality_index/const.py
+++ custom_components/worlds_air_quality_index/const.py
@@ -17,7 +17,7 @@ from homeassistant.components.sensor import SensorDeviceClass
 
 DOMAIN = "worlds_air_quality_index"
 PLATFORMS = [Platform.SENSOR]
-SW_VERSION = "1.0.5"
+SW_VERSION = "1.1.0"
 
 DEFAULT_NAME = 'waqi1'
 DISCOVERY_TYPE = "discovery_type"
